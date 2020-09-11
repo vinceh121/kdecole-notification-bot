ae1a685c63fd20ae2be8e4e0e05055d5b0233d67
vinceh121 <vincent.hyvert@sfr.fr>
2020-09-11 17:19:15 +0200

    better but slow af for no reason help command

--- src/main/java/me/vinceh121/knb/commands/CmdHelp.java
+++ src/main/java/me/vinceh121/knb/commands/CmdHelp.java
@@ -1,5 +1,9 @@
 package me.vinceh121.knb.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import me.vinceh121.knb.AbstractCommand;
 import me.vinceh121.knb.CommandContext;
 import me.vinceh121.knb.Knb;
@@ -8,13 +12,18 @@ public class CmdHelp extends AbstractCommand {
 
 	public CmdHelp(final Knb knb) {
 		super(knb);
+
 	}
 
 	@Override
 	protected void executeSync(final CommandContext ctx) {
 		final StringBuilder sb = new StringBuilder();
 		sb.append("```lisp\n");
-		for (final AbstractCommand cmd : this.knb.getCmdMap().values()) {
+
+		final List<AbstractCommand> cmds = new ArrayList<>(this.knb.getCmdMap().values());
+		Collections.sort(cmds, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+
+		for (final AbstractCommand cmd : cmds) {
 			sb.append("(");
 			sb.append(cmd.getName());
 			sb.append(")\t\t\t\t");
